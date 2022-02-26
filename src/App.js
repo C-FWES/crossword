a1f6ff03ba10b34a84558799206550e9ac1d21cc
Wesley Ou <69126784+C-FWES@users.noreply.github.com>
2022-02-26 09:47:40 -0500

    Input handling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import generateCrossword from "./GenerateCrossword";
+import React, { Component } from "react";
+import GenerateCrossword from "./GenerateCrossword";
+import { render } from "react-dom";
 
-function App() {
-  return (
-    <>
-    <div className="App">
-      <h1>Crossword Generator v0</h1>
-    </div>
-    <div class="container">
-      <div id="crossword-generate-elements">
-      <textarea rows='10'></textarea>
-      </div>
-      <button type="submit" onClick={generateCrossword}>Generate Crossword</button>
-    </div>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      givenWord: "",
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleInput.bind(this);
+  }
 
-    <div class="container">
-      <div id="crossword">
-        
+  handleInput(event) {
+    this.setState({givenWord: event.target.value})
+    console.log(event.target.value)
+  }
+
+  handleSubmit(event) {
+    console.log(this.state);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <>
+      <div className="App">
+        <h1>Crossword Generator v0</h1>
       </div>
-    </div>
-    </>
-  );
+      <div className="container">
+        <div id="crossword-generate-elements">
+        <textarea rows='10' onChange={this.handleInput} value={this.state.givenWord} />
+        </div>
+        <button type="submit" onClick={GenerateCrossword} onSubmit={this.handleSubmit}>Generate Crossword</button>
+      </div>
+  
+      <div className="container">
+        <div id="crossword">
+  
+        </div>
+      </div>
+      </>
+    );
+  }
 }
+
+render(<App/>, document.getElementById('root'));
 
 export default App;
