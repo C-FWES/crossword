104aa00ef39bf51003e5817b51df43ef6762478d
Wesley Ou <69126784+C-FWES@users.noreply.github.com>
2022-02-25 20:19:50 -0500

    Barebones code
    This weekend's project, as discussed in history class

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Crossword Generator v0</h1>
     </div>
+    <div class="container">
+      <div id="crossword-generate-elements">
+      <textarea rows='10'></textarea>
+      </div>
+      <button type="submit">Generate Crossword</button>
+    </div>
+    </>
   );
 }
 
